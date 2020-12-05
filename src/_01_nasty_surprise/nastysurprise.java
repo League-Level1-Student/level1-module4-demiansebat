a29f81cb507c967a7872d3f8b572ea3a742e3cc7
root <root@3aaddd3d2c84>
2020-12-05 00:54:58 +0000

    nasty surprise ip

---
+++ src/_01_nasty_surprise/nastysurprise.java
@@ -0,0 +1,11 @@
+package _01_nasty_surprise;
+
+import javax.swing.JButton;
+
+public class nastysurprise {
+JButton button= new JButton("Trick");
+JButton handle= new JButton("Treat");
+JOptionPane.showMessageDialogue(""){
+	
+}
+}
