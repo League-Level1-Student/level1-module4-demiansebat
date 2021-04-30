ca0b5123426074ca113d3943c55d3ea452033017
league <league@imac-25.attlocal.net>
2021-04-30 16:55:43 -0700

    calculator ip

--- src/_08_calculator/Calculator.java
+++ src/_08_calculator/Calculator.java
@@ -1,5 +1,68 @@
 package _08_calculator;
 
-public class Calculator {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+public class Calculator implements ActionListener{
+	public void setup() {
+		
+	addit();
+	subt();
+	mult();
+	divid();
+	JFrame frame=new JFrame("Simple Calculator");
+	JPanel panel= new JPanel();
+	JButton add= new JButton("Add");
+	JButton sub= new JButton("Sub");
+	JButton mul= new JButton("Mul");
+	JButton div= new JButton("Div");
+	JTextField input= new JTextField(10);
+	JTextField text= new JTextField(10);
+	frame.add(panel);
+	panel.add(add);
+	panel.add(sub);
+	panel.add(mul);
+	panel.add(div);
+	panel.add(input);
+	panel.add(text);
+	frame.setVisible(true);
+	frame.pack();
+	add.addActionListener(this);
+	mul.addActionListener(this);
+	sub.addActionListener(this);
+	div.addActionListener(this);
+	frame.setSize(350, 300);
+	
+	}
+
+	private void divid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void mult() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void subt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addit() {
+		// TODO Auto-generated method stub
+	}
+		
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}	
 }
