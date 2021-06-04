8fa4b3ab6b284a1e8a6d1e39ed2252193cd4253d
league <league@imac-25.attlocal.net>
2021-06-04 16:57:39 -0700

    whack a mole ip

---
+++ src/_11_whack_a_mole/molerunner.java
@@ -0,0 +1,9 @@
+package _11_whack_a_mole;
+
+import _11_whack_a_mole.whackamole;
+
+public class molerunner {
+	public static void main(String[] args) {
+		new whackamole().setup();
+	}
+}
--- src/_11_whack_a_mole/whackamole.java
+++ src/_11_whack_a_mole/whackamole.java
@@ -1,5 +1,61 @@
 package _11_whack_a_mole;
 
-public class whackamole {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class whackamole implements ActionListener {
+	Random randy= new Random();
+	JButton molebutton;
+public void setup() {
+	JFrame frame= new JFrame ("Whack a Button!");
+	JPanel panel= new JPanel();
+	frame.add(panel);
+	frame.setVisible(true);
+	frame.setSize(280, 300);
+	int moleposition= randy.nextInt(24);
+	for (int i = 0; i < 24; i++) {
+		JButton button= new JButton();
+		panel.add(button);
+		button.addActionListener(this);
+		if(i== moleposition) {
+			button.setText("Mole!");
+			molebutton= button;
+			
+		}
+	}
+	frame.pack();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+if (e.getSource()==molebutton) {
+		speak( "Correct! I guess your not an ablsolute idiot after all!");
+	}
+else {
+	speak("How dumb could you possibly be?");
+}
+}
+static void speak(String words) {
+    if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+        String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+                + words + "');\"";
+        try {
+            Runtime.getRuntime().exec( cmd ).waitFor();
+        } catch( Exception e ) {
+            e.printStackTrace();
+        }
+    } else {
+        try {
+            Runtime.getRuntime().exec( "say " + words ).waitFor();
+        } catch( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+}
 }
